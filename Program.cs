555ddbfa9c1d747ac282ab2cf8573c9a7905855e
jeangalvis <campusjean2023@gmail.com>
2023-12-04 15:08:42 -0500

    fibonacci

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Ingrese el numero: ");
+int valor = Convert.ToInt32(Console.ReadLine());
+int validador = 0;
+int temporal1 = 0;
+int temporal2 = 1;
+while(validador != 1){
+    int suma = temporal1 + temporal2;
+    temporal1 = temporal2;
+    temporal2 = suma;
+    if(valor == temporal2){
+        validador = 1;
+        Console.WriteLine(valor + " es numero de fibonacci");
+    }
+    else if(temporal2 > valor){
+        validador = 1;
+        Console.WriteLine(valor+ " no es numero de fibonacci");
+    }
+}
